3e584b860a2b6d423e91dfa0566519ff98da728a
Alex Eager <156651956+aweager@users.noreply.github.com>
2024-06-24 04:04:51 -0700

    switch rest of lib to new param names

--- lib/system.zsh
+++ lib/system.zsh
@@ -1,61 +1,66 @@
-function __mux-parse-and-call-system() {
-    if [[ $# -ne 0 ]]; then
-        echo "mux system calls have no args" >&2
-        return 1
-    fi
+() {
+    local no_args="$(__mux-build-validator)"
 
-    "__mux-system-${cmd_name}"
-}
-
-function __mux-system-sync-registers-down() {
-    private parent_mux_cmd="$("${cmd_prefix}get-parent-mux")"
-    if [[ -z $parent_mux_cmd ]]; then
-        echo "No parent mux" >&2
-        return 1
-    fi
-
-    private -a parent_regnames
-    parent_regnames=($(eval "$parent_mux_cmd" list-registers))
-
-    private regname
-    for regname in $parent_regnames; do
-        private value="$(eval "$parent_mux_cmd" get-register $regname)"
-        MuxArgs[regname]="$regname"
-        MuxArgs[value]="$value"
-        "${cmd_prefix}set-register"
-    done
-
-    local -a my_regnames
-    my_regnames=($("${cmd_prefix}list-registers"))
-
-    for regname in $my_regnames; do
-        if ! (($parent_regnames[(Ie)$regname])); then
-            "${cmd_prefix}delete-register"
+    functions[mux-validate-sync-registers-down]="$no_args"
+    function mux-exec-sync-registers-down() {
+        private parent_mux_cmd="$(mux-impl-get-parent-mux)"
+        if [[ -z $parent_mux_cmd ]]; then
+            echo "No parent mux" >&2
+            return 1
         fi
-    done
-}
 
-function __mux-system-sync-registers-up() {
-    local child_mux_cmd="$("${cmd_prefix}get-child-mux")"
-    if [[ -z $child_mux_cmd ]]; then
-        echo "No child mux" >&2
-        return 1
-    fi
+        private -a parent_regnames
+        parent_regnames=($(eval "$parent_mux_cmd list-registers"))
 
-    local -a child_regnames
-    child_regnames=($(eval "$child_mux_cmd" list-registers))
+        private regname
+        for regname in $parent_regnames; do
+            private value="$(eval "$parent_mux_cmd get-register $regname")"
+            MuxArgs[regname]="$regname"
+            MuxArgs[value]="$value"
+            "mux-impl-set-register"
+        done
 
-    for regname in $child_regnames; do
-        local value="$(eval "$child_mux_cmd" get-register $regname)"
-        "${cmd_prefix}set-register"
-    done
+        unset "MuxArgs[value]"
 
-    local -a my_regnames
-    my_regnames=($("${cmd_prefix}list-registers"))
+        private -a my_regnames
+        my_regnames=($(mux-impl-list-registers))
 
-    for regname in $my_regnames; do
-        if ! (($child_regnames[(Ie)$regname])); then
-            "${cmd_prefix}delete-register"
+        for regname in $my_regnames; do
+            if ! (($parent_regnames[(Ie)$regname])); then
+                MuxArgs[regname]="$regname"
+                mux-impl-delete-register
+            fi
+        done
+    }
+
+    functions[mux-validate-sync-registers-up]="$no_args"
+    function mux-exec-sync-registers-up() {
+        private child_mux_cmd="$(mux-impl-get-child-mux)"
+        if [[ -z $child_mux_cmd ]]; then
+            echo "No child mux" >&2
+            return 1
         fi
-    done
+
+        private -a child_regnames
+        child_regnames=($(eval "$child_mux_cmd" list-registers))
+
+        private regname
+        for regname in $child_regnames; do
+            MuxArgs[regname]="$regname"
+            MuxArgs[value]="$(eval "$child_mux_cmd get-register $regname")"
+            mux-impl-set-register
+        done
+
+        unset "MuxArgs[value]"
+
+        private -a my_regnames
+        my_regnames=($(mux-impl-list-registers))
+
+        for regname in $my_regnames; do
+            if ! (($child_regnames[(Ie)$regname])); then
+                MuxArgs[regname]="$regname"
+                mux-impl-delete-register
+            fi
+        done
+    }
 }
--- lib/tree.zsh
+++ lib/tree.zsh
@@ -1,15 +1,49 @@
-function __mux-parse-and-call-muxcmd-only() {
-    eval "$(
+() {
+    functions[mux-validate-register-child-mux]="$(
         __mux-build-validator \
+            -b \
+            --location \
             muxcmd
     )"
 
-    "${cmd_prefix}${cmd_name}"
-}
+    function mux-exec-register-child-mux() {
+        mix-impl-register-child-mux
+    }
 
-function __mux-validate-muxcmd() {
-    if [[ "$1" =~ ";" ]]; then
-        echo "Mux commands cannot contain ';'" >&2
-        return 1
-    fi
+    local location="$(
+        __mux-build-validator \
+            -b \
+            --location
+    )"
+
+    functions[mux-validate-unregister-child-mux]="$location"
+    function mux-exec-unregister-child-mux() {
+        mux-impl-unregister-child-mux
+    }
+
+    functions[mux-validate-get-child-mux]="$location"
+    function mux-exec-get-child-mux() {
+        mux-impl-get-child-mux
+    }
+
+    local no_args="$(__mux-build-validator)"
+
+    functions[mux-validate-get-parent-mux]="$no_args"
+    function mux-exec-get-parent-mux() {
+        mux-impl-get-parent-mux
+    }
+
+    functions[mux-validate-get-mux-cmd]="$no_args"
+    function mux-exec-get-mux-cmd() {
+        mux-impl-get-mux-cmd
+    }
+
+    function __mux-validate-muxcmd() {
+        MuxArgs[muxcmd]="$1"
+
+        if [[ "$1" =~ ";" ]]; then
+            echo "Mux commands cannot contain ';'" >&2
+            return 1
+        fi
+    }
 }
